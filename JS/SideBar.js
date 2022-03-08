da2cc2c38edb8118eb5562423f1a05ba261d8e88
Dhruv Rayat <dhruvrayat50@gmail.com>
2022-03-08 14:28:46 +1300

    Added header

---
+++ JS/SideBar.js
@@ -0,0 +1,9 @@
+/* Set the width of the side navigation to 100VW */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "100vw";
+  }
+  
+  /* Set the width of the side navigation to 0 */
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
